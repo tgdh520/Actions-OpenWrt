6439b96a860cff20a903b78f0ef3828fc52d21e2
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2025-03-07 22:21:11 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+touch immortalwrt-mt798x-23.05/target/linux/*/Makefile
